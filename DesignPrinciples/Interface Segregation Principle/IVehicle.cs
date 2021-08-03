e608bd7c3a874f3794cfd1db78a001cd532a6e6f
[Sona] <[sona16061999@gmail.com]>
2021-08-03 08:02:47 +0530

    [Sona] Interface Segregation Principle

---
+++ DesignPrinciples/Interface Segregation Principle/IVehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPrinciples.Interface_Segregation_Principle
+{
+    public interface ICar
+    {
+        void Drive();
+    }
+    public interface IAirplane
+    {
+        void Fly();
+    }
+}
